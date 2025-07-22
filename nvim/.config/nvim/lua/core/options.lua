5b11226490f518c9a0b82e84fa85565247df577d
MatejKorz <korz.matej@gmail.com>
2025-07-22 17:58:26 +0200

    feat: added undotree in nvim

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -2,6 +2,8 @@ local opt = vim.opt
 
 opt.relativenumber = false
 opt.number = true
+opt.scrolloff = 10
+opt.sidescrolloff = 6
 
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -14,3 +16,8 @@ opt.termguicolors = true
 opt.signcolumn = "yes"
 opt.colorcolumn = "80,110,140"
 opt.cursorline = true
+
+opt.undofile = true
+opt.undodir = vim.fn.expand("~/.local/share/nvim/undo")
+
+opt.errorbells = false
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,73 +1,76 @@
 return {
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'saghen/blink.cmp' },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          local bufnr = args.buf
-          local opts = { buffer = bufnr }
-          local keymap = vim.keymap
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = { 'saghen/blink.cmp' },
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    local bufnr = args.buf
+                    local opts = { buffer = bufnr }
+                    local keymap = vim.keymap
 
-          opts.desc = "Show LSP references"
-          keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+                    opts.desc = "Show LSP references"
+                    keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-          opts.desc = "Go to declaration"
-          keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+                    opts.desc = "Go to declaration"
+                    keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-          opts.desc = "Show LSP definitions"
-          keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+                    opts.desc = "Show LSP definitions"
+                    keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-          opts.desc = "Show LSP type definitions"
-          keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+                    opts.desc = "Show LSP type definitions"
+                    keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-          opts.desc = "See available code actions"
-          keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+                    opts.desc = "See available code actions"
+                    keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-          opts.desc = "Smart rename"
-          keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+                    opts.desc = "Smart rename"
+                    keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-          opts.desc = "Show buffer diagnostics"
-          keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+                    opts.desc = "Show buffer diagnostics"
+                    keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-          opts.desc = "Show line diagnostics"
-          keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+                    opts.desc = "Show line diagnostics"
+                    keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-          opts.desc = "Show documentation for what is under cursor"
-          keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+                    opts.desc = "Show documentation for what is under cursor"
+                    keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-          opts.desc = "Restart LSP"
-          keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+                    opts.desc = "Restart LSP"
+                    keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 
-          opts.desc = "Format code"
-          keymap.set("n", "<leader>fc", function()
-            vim.lsp.buf.format({ async = true })
-          end, opts)
+                    opts.desc = "Format code"
+                    keymap.set("n", "<leader>fc", function()
+                        vim.lsp.buf.format({ async = true })
+                    end, opts)
+                end
+            })
+
+            local lspconfig = require('lspconfig')
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+            lspconfig['lua_ls'].setup({
+                settings = {
+                    Lua = { diagnostics = { globals = { "vim" } } }
+                },
+                capabilities = capabilities
+            })
+
+            lspconfig['clangd'].setup({
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--header-insertion=never"
+                },
+                capabilities = capabilities
+            })
+
+            lspconfig['gopls'].setup({
+                capabilities = capabilities
+            })
         end
-      })
-
-      local lspconfig = require('lspconfig')
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-      lspconfig.clangd.setup({
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=never"
-        },
-      })
-
-      lspconfig.lua_ls.setup({
-        settings = {
-          Lua = { diagnostics = { globals = { "vim" } } }
-        },
-      })
-
-      lspconfig['lua_ls'].setup({ capabilities = capabilities })
-      lspconfig['clangd'].setup({ capabilities = capabilities })
-    end
-  }
+    }
 }
