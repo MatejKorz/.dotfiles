21b794f55913930e7a0e29b61f43fb88ae854229
MatejKorz <korz.matej@gmail.com>
2025-08-18 20:57:37 +0200

    feat: dynamic wallpaper switching on sway

---
+++ sway/.config/sway/scripts/dynamic_wallpaper.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+swaybg -i $(find $HOME/wallpaper_dir/. -type f | shuf -n1) -m fill &
+OLD_PID=$!
+while true; do
+    sleep 600
+    swaybg -i $(find $HOME/wallpaper_dir/. -type f | shuf -n1) -m fill &
+    NEXT_PID=$!
+    sleep 5
+    kill $OLD_PID
+    OLD_PID=$NEXT_PID
+done
